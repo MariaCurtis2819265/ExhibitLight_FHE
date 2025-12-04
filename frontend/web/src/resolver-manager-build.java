e3e349403b28b7607b177994e8e3d47340d51037
MariaCurtis2819265 <ioanna_v187526309@rambler.ru>
2025-12-04 13:03:12 +0800

    add order-user-inventory.adoc, hardhat.d.ts, service-router-model.py

---
+++ frontend/web/src/resolver-manager-build.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\resolver-manager-build.java
+// Generated at: 2025-10-10T12:35:43.642Z
---
+++ frontend/web/src/service-router-model.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\service-router-model.py
+// Generated at: 2025-10-10T12:35:43.635Z
